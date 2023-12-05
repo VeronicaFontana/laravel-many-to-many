f22dd97d2901bdff1d1a90b6ee19cad9cc634930
VeronicaFontana <fontana.veronica98@gmail.com>
2023-12-05 12:06:09 +0100

    [ADD] User name in header

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -2,10 +2,14 @@
     <nav class="navbar navbar-dark">
         <div class="container-fluid">
             <a href="{{ route("home") }}" target="_blank" class="navbar-brand fs-4 btn">Vai al sito <i class="fa-solid fa-square-arrow-up-right"></i></a>
-            <form action="{{ route('logout') }}" method="POST" class="d-flex" role="search">
-                @csrf
-                <button class="btn" type="submit"><i class="fa-solid fa-right-from-bracket"></i></button>
-            </form>
+            <div class="d-flex">
+                <a href="{{ route("profile.edit") }}" class="text-white text-decoration-none m-auto me-2">{{ Auth::user()->name }}</a>
+                <form action="{{ route('logout') }}" method="POST" class="d-flex" role="search">
+                    @csrf
+                    <button class="btn" type="submit"><i class="fa-solid fa-right-from-bracket"></i></button>
+                </form>
+            </div>
+
         </div>
     </nav>
 </header>
